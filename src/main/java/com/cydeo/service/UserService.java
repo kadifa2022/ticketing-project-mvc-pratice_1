ef35e5ee21366d7494725757b0499f26f0b1d20c
Kadifa <Kudickadifa@gmail.com>
2022-10-06 23:33:37 -0500

    ticketing project practice1

--- src/main/java/com/cydeo/service/UserService.java
+++ src/main/java/com/cydeo/service/UserService.java
@@ -2,13 +2,13 @@ package com.cydeo.service;
 
 import com.cydeo.dto.UserDTO;
 
-import java.util.List;
 
-public interface UserService {
 
+public interface UserService extends CrudService<UserDTO, String> {
+/*
     UserDTO save(UserDTO user);
     UserDTO findById(String username);
     List<UserDTO> findAll();
     void deleteById(String username);
-
+*/
 }
