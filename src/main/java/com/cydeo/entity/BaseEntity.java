c667aa97f9312a4e715ffca20f8253bdbb7d1a93
Kadifa <Kudickadifa@gmail.com>
2022-10-06 22:22:45 -0500

    ticketing project practice1

---
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -0,0 +1,15 @@
+package com.cydeo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BaseEntity {
+    private Long id;
+    private LocalDateTime insertDataTime;
+    private Long insertUserId;
+    private LocalDateTime lastUpdateDateTime;
+}
