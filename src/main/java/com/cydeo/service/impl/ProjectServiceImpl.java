81299054764f51a572f1fb3c366c73c9f80be734
Kadifa <Kudickadifa@gmail.com>
2022-10-09 09:15:19 -0500

    ticketing project practice1

--- src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -29,6 +29,16 @@ public class ProjectServiceImpl extends AbstractMapService<ProjectDTO, String>im
         return super.findAll();
     }
 
+
+
+    @Override
+    public void update(ProjectDTO object) {
+        if(object.getProjectStatus()==null){
+            object.setProjectStatus(findById(object.getProjectCode()).getProjectStatus());
+        }
+        super.update(object.getProjectCode(), object);
+
+    }
     @Override
     public void deleteById(String projectCode) {
         super.deleteById(projectCode);
@@ -36,8 +46,8 @@ public class ProjectServiceImpl extends AbstractMapService<ProjectDTO, String>im
     }
 
     @Override
-    public void update(ProjectDTO object) {
-        super.update(object.getProjectCode(), object);
+    public void complete(ProjectDTO project) {
+        project.setProjectStatus(Status.COMPLETE);
 
     }
 }
