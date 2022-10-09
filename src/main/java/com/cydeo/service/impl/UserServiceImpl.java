ac488a1c71bd0008e55b1678413d90c0e0ae3587
Kadifa <Kudickadifa@gmail.com>
2022-10-09 09:15:34 -0500

    ticketing project practice1

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -5,6 +5,8 @@ import com.cydeo.service.UserService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class UserServiceImpl extends AbstractMapService<UserDTO, String> implements UserService {
     @Override
@@ -33,4 +35,9 @@ public class UserServiceImpl extends AbstractMapService<UserDTO, String> impleme
         super.update(object.getUserName(),object);
 
     }
+
+    @Override
+    public List<UserDTO> findManagers() {
+       // return super.findAll();
+        return findAll().stream().filter(user->user.getRole().getId()==2).collect(Collectors.toList());}
 }
