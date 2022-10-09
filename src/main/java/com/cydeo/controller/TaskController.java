cd2ae3399f8ab2ef557ad3946eeb6d747555756c
Kadifa <Kudickadifa@gmail.com>
2022-10-09 13:50:17 -0500

    ticketing project practice1

--- src/main/java/com/cydeo/controller/TaskController.java
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -1,21 +1,43 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.TaskDTO;
+import com.cydeo.service.ProjectService;
+import com.cydeo.service.TaskService;
+import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/task")
 public class TaskController {
+    private final UserService userService;
+    private final ProjectService projectService;
+    private final TaskService taskService;
+
+    public TaskController(UserService userService, ProjectService projectService, TaskService taskService) {
+        this.userService = userService;
+        this.projectService = projectService;
+        this.taskService = taskService;
+    }
 
     @GetMapping("/create")
     public String createTask(Model model) {
-
-
-
+        model.addAttribute("task", new TaskDTO());
+        model.addAttribute("projects", projectService.findAll());
+        model.addAttribute("employees", userService.findEmployees());
+        model.addAttribute("tasks", taskService.findAll());
 
         return "task/create";
+
+    }
+    @PostMapping("/create")
+    public String insertTask(TaskDTO task){
+        taskService.save(task);
+
+        return "redirect:/task/create";
 
     }
 }
