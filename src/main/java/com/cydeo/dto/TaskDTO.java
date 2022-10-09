6201b0d4b1f49e03dd9cab0b712a1e6b809a3797
Kadifa <Kudickadifa@gmail.com>
2022-10-09 10:48:43 -0500

    ticketing project practice1

---
+++ src/main/java/com/cydeo/dto/TaskDTO.java
@@ -0,0 +1,25 @@
+package com.cydeo.dto;
+
+import com.cydeo.enums.Status;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TaskDTO {
+   private Long id; //if we don't have anything unique, developer has to provide based on business logic like this one Long id
+
+    private ProjectDTO project;
+    private UserDTO assignedEmployee;
+    private String taskSubject;
+    private String taskDetail;
+    private Status taskStatus;
+    private LocalDate assignedDate;
+
+
+
+}
