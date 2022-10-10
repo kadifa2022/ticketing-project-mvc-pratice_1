19712d9c47a4f5876c70e11a00ba76091af5a815
Kadifa <Kudickadifa@gmail.com>
2022-10-09 22:20:15 -0500

    ticketing project practice1

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/project")
 public class ProjectController {
@@ -69,6 +71,15 @@ public class ProjectController {
 
         return "redirect:/project/create";
     }
+    @GetMapping("/manager/project-status")
+    public String getProjectByManager(Model model){
 
+        UserDTO manager =userService.findById("john@cydeo.com");//no security-hard coded dor now
+        List<ProjectDTO> projects=projectService.getCountedListOfProjectDTO(manager);
+
+       model.addAttribute("projects",projects);
+        return "/manager/project-status";
+
+    }
 
 }
